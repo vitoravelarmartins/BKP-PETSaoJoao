68f17b653051b1f04076bf4da7bec71d4eca6f30
vitoravelarmartins <vitor.martins@sou.unifeob.edu.br>
2020-04-17 08:09:05 -0300

    ReabaseForCommits

---
+++ lib/components/comp_cad_tutor/sizebox.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Widget sizebox(valor) {
+  return SizedBox(
+    height: valor,
+  );
+}
--- lib/pages/cad_tutor/cadastro_tutor.dart
+++ lib/pages/cad_tutor/cadastro_tutor.dart
@@ -1,11 +1,81 @@
-import 'package:flutter/material.dart';
 //Acompanhe desing do projeto aqui --> https://www.figma.com/file/GYFrt79mzIbOUXXmFyDgwL/Material-Baseline-Design-Kit?node-id=38%3A5814
 
-class cadastro_tutor extends StatelessWidget {
+import 'dart:async';
+import 'package:flutter/services.dart';
+
+import 'package:flutter/material.dart';
+import 'package:petsaojoao/pages/cad_tutor/info_pessoal.dart';
+
+class cadastro_tutor extends StatefulWidget {
+  @override
+  _cadastro_tutorState createState() => _cadastro_tutorState();
+}
+
+class _cadastro_tutorState extends State<cadastro_tutor> {
+  void navigationToNextPage() {
+    // Navigator.push(context, _createRoute());
+    Navigator.pushReplacement(context, _createRoute());
+  }
+
+  startCadTutorTimer() async {
+    var _duration = new Duration(seconds: 5);
+    return new Timer(_duration, navigationToNextPage);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startCadTutorTimer();
+  }
+
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(appBar: AppBar(title: Text('Cadastro Tutor'))));
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          backgroundColor: Colors.blueAccent[200],
+          body: ListView(children: <Widget>[
+            Container(child: Image.asset('assets/background/superior.png')),
+            SizedBox(
+              height: 100,
+            ),
+            Center(
+              child: Container(
+                width: 350,
+                child: Text(
+                  'Ops, não temos seus dados ainda 😥😿',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            Container(child: Image.asset('assets/background/inferior.png')),
+          ])),
+    );
   }
+}
+
+Route _createRoute() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => Info_pessoal(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      var begin = Offset(0.0, 1.0);
+      var end = Offset.zero;
+      var curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
 }
